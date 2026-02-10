22ca344e86399542c2de53c07b34833a5c4a76da
Johann Patrício Daniel <johannpatriciod@gmail.com>
2026-02-10 13:41:57 -0300

    fazendo testes de novo

--- backstopConfig.js
+++ backstopConfig.js
@@ -1,14 +1,18 @@
 'use strict';
+// https://github.com/garris/BackstopJS#advanced-scenarios
 
 const backstop = require('@mate-academy/backstop-config');
 const { basicScenario } = backstop;
 
 const basic = {
   ...basicScenario,
+  label: 'Elementary test',
   selectors: ['body'],
-  removeSelectors: ['h1'],
+  removeSelectors: [
+    'h1',
+  ],
   misMatchThreshold: 0.5,
-  referenceUrl: basicScenario.referenceUrl,
+  referenceUrl: 'https://johannpdaniel.github.io/layout_calendar/',
 };
 
 const config = {
@@ -27,27 +31,21 @@ const config = {
     {
       ...basic,
       label: 'Calendar initial state',
-      url: 'https://johannpdaniel.github.io/layout_calendar/',
     },
     {
       ...basic,
       label: 'Calendar day with hover effect',
-      url: 'https://johannpdaniel.github.io/layout_calendar/',
       hoverSelector: '.calendar__day:nth-child(5)',
       postDOMChangeWait: 1000,
     },
     {
       ...basic,
       label: 'Calendar starting from Wednesday',
-      url: 'https://johannpdaniel.github.io/layout_calendar/',
-      selectors: ['.calendar'],
       postDOMChangeWait: 1000,
     },
     {
       ...basic,
       label: 'Calendar with length of 29 days',
-      url: 'https://johannpdaniel.github.io/layout_calendar/',
-      selectors: ['.calendar'],
       postDOMChangeWait: 1000,
     },
   ],
